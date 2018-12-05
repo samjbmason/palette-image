595a2792e02918f38e2bc62602e656e9d8ac08ac
Sam Mason <smason@cloudflare.com>
2018-12-05 15:06:26 +0000

    Fix issue with only post running function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const { buffer } = require("micro")
+const { buffer, send } = require("micro")
+const microCors = require("micro-cors")
 const getColors = require("get-image-colors")
 const imageType = require("image-type")
 
-module.exports = async (req, res) => {
+const cors = microCors()
+
+module.exports = cors(async (req, res) => {
+  console.log(req.method)
+  if (req.method !== "POST") {
+    return send(res, 200)
+  }
   const buf = await buffer(req)
   const type = imageType(buf)
   const colors = await getColors(buf, type.mime)
-  return colors.map(c => c.hex())
-}
+  send(res, 200, colors.map(c => c.hex()))
+})
