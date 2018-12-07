508192ccfa7a996ca0a215f1f299ca3d1cba4d79
Sam Mason <smason@cloudflare.com>
2018-12-07 13:50:02 +0000

    Bump image limit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = cors(async (req, res) => {
   if (req.method !== "POST") {
     return send(res, 200)
   }
-  const buf = await buffer(req)
+  const buf = await buffer(req, { limit: "5mb" })
   const type = imageType(buf)
   const colors = await getColors(buf, type.mime, 11)
   send(res, 200, colors.map(c => c.hex()))
